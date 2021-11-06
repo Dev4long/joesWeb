41c8ed530d735094dd835f00756c118b477f2cd7
deniskadrich <dev4long@live.com>
2021-11-06 16:22:07 -0400

    ok

--- my-app/src/Navbar.js
+++ my-app/src/Navbar.js
@@ -3,14 +3,7 @@ import React, {useState} from 'react'
 import { Link } from "react-router-dom";
 import * as FaIcons from "react-icons/fa";
 import * as AiIcons from "react-icons/ai";
-import * as HiIcons from "react-icons/hi";
-import * as MdIcons from "react-icons/md";
-import * as GiIcons from "react-icons/gi";
 import * as CgIcons from "react-icons/cg";
-import * as FiIcons from "react-icons/fi";
-import * as BiIcons from "react-icons/bi";
-import * as BsIcons from "react-icons/bs";
-import * as RiIcons from "react-icons/ri";
 
 
 
@@ -45,41 +38,6 @@ function Navbar() {
                     <Link to='/portfolio' className="icons">
                     <AiIcons.AiFillFolderOpen />Portfolio
                     </Link>
-                    {/* <li>
-                    <Link to='/events' className="icons">
-                    <RiIcons.RiCalendarEventFill/>Local events
-                    </Link>
-                    <Link to='/login' className="icons">
-                    <HiIcons.HiOutlineLogin />Login/Signup
-                    </Link>
-                    <Link to='/bikes' className="icons">
-                    <MdIcons.MdDirectionsBike />Bikes
-                    </Link>
-                    <Link to='/skateboards' className="icons">
-                    <GiIcons.GiSkateboard/>Skateboards
-                    </Link>
-                    <Link to='/skates' className="icons">
-                    <GiIcons.GiRollerSkate />Skates
-                    </Link>
-                    <Link to='/cart' className="icons">
-                    <HiIcons.HiOutlineShoppingCart />My cart
-                    </Link>
-                    <Link to='/purchase' className="icons">
-                    <BiIcons.BiPurchaseTag />My purchases
-                    </Link>
-                    <Link to='/profile' className="icons">
-                    <CgIcons.CgProfile />My profile
-                    </Link>
-                    <Link to='/map' className="icons">
-                    <FiIcons.FiMapPin/> Events Google maps
-                    </Link>
-                    <Link to='/chat' className="icons">
-                    <BsIcons.BsChatDots/>Connect
-                    </Link>
-                    
-                    
-                    
-                    </li> */}
                 </ul>
             </nav>
         </div>
